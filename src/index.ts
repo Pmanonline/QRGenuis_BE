a7169a4fff858a298c4f13dc77a4f96e48974083
Pmanonline <petersonzoconis@gmail.com>
2025-10-24 16:14:48 +0100

    pushing first changes

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,6 @@ import { errorHandler } from "./utilities/errorHandler.util";
 import { options as prodOptions } from "./prodSwagger";
 import { options as devOptions } from "./devSwagger";
 import deserializeUser from "./middlewares/deserializeUser.middleware";
-//import protectRoutes from "./middlewares/protectRoutes.middleware";
-//import individualRoutes from "./modules/users/individualUsers/individualUsers.route";
 import individualUsersRoutes from "./modules/users/individualUsers/individualUsers.route";
 import organizationUsersRoutes from "./modules/users/organization/getOrganizationUser.route";
 import authRouter from "./modules/authentication/userAuth.route";
@@ -59,7 +57,7 @@ app.use("/auth/organization", organizationUserAuthRouter);
 app.use("/auth/individual", individualUserAuthRouter);
 app.use("/auth", authRouter);
 app.use("/user", organizationUsersRoutes);
-app.use("/users", individualUsersRoutes);
+app.use("/api", individualUsersRoutes);
 
 // transaction
 app.use("/transactions", escrowProductTransactionRouter);
